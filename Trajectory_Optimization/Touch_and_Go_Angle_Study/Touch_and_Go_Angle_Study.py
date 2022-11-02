77915864c62e67ffcea864362d3e9dc9d328d0b8
Matthew Clarke <mclarke2@stanford.edu>
2022-11-02 17:17:47 -0400

    Update Touch_and_Go_Angle_Study.py

--- Trajectory_Optimization/Touch_and_Go_Angle_Study/Touch_and_Go_Angle_Study.py
+++ Trajectory_Optimization/Touch_and_Go_Angle_Study/Touch_and_Go_Angle_Study.py
@@ -39,21 +39,21 @@ def main():
 
     
     # SET UP SIMULATION PARAMETERS     
-    simulated_days    = 1               # number of days simulated 
-    flights_per_day   = 1               # number of flights per day 
-    aircraft_range    = 55 *Units.nmi   # total ground distance 
-    reserve_segment   = False           # reserve segment flag, if true, ground distance not included  
-    recharge_battery  = False           # flag to simulate battery recharge  
-    plot_mission      = False           # plot mission flag  
-    control_points    = 10              # number of control points per segment 
-    true_course_angles= np.array([0,45,90,135,180])  # in degrees 
-    run_noise_model   = True            # flag to run noise analysis    
-    N_gm_x            = 10              # number of microphones in longitudinal direction on ground 
-    N_gm_y            = 5               # number of microphones in lateral direction on ground  
-    min_y             = 1E-3            # minimum y (lateral) coordinate of acoustic computational domain 
-    max_y             = 2.5*Units.nmi   # maxiumum y (lateral) coordinate of acoustic computational domain
-    min_x             = 0.*Units.nmi    # minimum x (longitudinal) coordinate of acoustic computational domain 
-    max_x             = 4.35*Units.nm   # maxiumum x (longitudinal) coordinate of acoustic computational domain 
+    simulated_days        = 1               # number of days simulated 
+    flights_per_day       = 1               # number of flights per day 
+    aircraft_range        = 55 *Units.nmi   # total ground distance 
+    reserve_segment       = False           # reserve segment flag, if true, ground distance not included  
+    recharge_battery      = False           # flag to simulate battery recharge  
+    plot_mission          = False           # plot mission flag  
+    control_points        = 10              # number of control points per segment 
+    true_course_angle_deg = 0               # in degrees 
+    run_noise_model       = True            # flag to run noise analysis    
+    N_gm_x                = 5               # number of microphones in longitudinal direction on ground 
+    N_gm_y                = 5               # number of microphones in lateral direction on ground  
+    min_y                 = -2.5*Units.nmi  # minimum y (lateral) coordinate of acoustic computational domain 
+    max_y                 = 2.5*Units.nmi + 1E-2  # maxiumum y (lateral) coordinate of acoustic computational domain
+    min_x                 = 0.*Units.nmi    # minimum x (longitudinal) coordinate of acoustic computational domain 
+    max_x                 = 4.35*Units.nm   # maxiumum x (longitudinal) coordinate of acoustic computational domain 
     
     
     # SET UP VEHICLE 
@@ -65,60 +65,59 @@ def main():
     
     # devide computational domain to increase noise prediction
     if run_noise_model:
-        n = 3  
+        n = 5  
     else:
         n = 2
     Y_LIM = np.linspace(min_y,max_y,n)     
-    X_LIM = np.linspace(min_x,max_x,n)     
-    for ang in range(len(true_course_angles)): 
-        Q_idx             = 1        
-        true_course_angle = true_course_angles[ang]* Units.degrees
-        for i in range(len(X_LIM)-1):
-            for j in range(len(Y_LIM)-1): 
-                # start simulation clock
-                ti                = time.time() 
-                
-                print('Running Quardant:' + str(Q_idx) + ' Angle: ' + str(true_course_angles[ang]) ) 
-                min_x = X_LIM[i]
-                max_x = X_LIM[i+1]
-                min_y = Y_LIM[j]
-                max_y = Y_LIM[j+1]
+    X_LIM = np.linspace(min_x,max_x,n)      
+    Q_idx             = 1        
+    true_course_angle = true_course_angle_deg * Units.degrees
+    for i in range(len(X_LIM)-1):
+        for j in range(len(Y_LIM)-1): 
+            # start simulation clock
+            ti                = time.time() 
             
-                configs_analyses  = Stopped_Rotor_V2_Analyses.analyses_setup(configs,N_gm_x,N_gm_y,min_y,max_y,min_x,max_x,
-                                                   aircraft_range,run_noise_model) 
-                
-                # SET UP MISSION PROFILE 
-                base_mission      = Stopped_Rotor_V2_Missions.approach_departure_mission_setup(configs_analyses,vehicle,simulated_days,flights_per_day,
-                                                        aircraft_range,reserve_segment,control_points,recharge_battery,true_course_angle )
-                missions_analyses = Stopped_Rotor_V2_Missions.missions_setup(base_mission) 
-                
-                # DEFINE ANALYSES 
-                analyses          = SUAVE.Analyses.Analysis.Container()
-                analyses.configs  = configs_analyses
-                analyses.missions = missions_analyses 
-                
-                # FINALIZE SIMULATION 
-                configs.finalize()
-                analyses.finalize()     
-                
-                # APPEND MISSION TO SIMULATION 
-                mission           = analyses.missions.base
-                
-                # RUN SIMULATION !!
-                noise_results     = mission.evaluate()   
-                
-                # SAVE RESULTS
-                filename          = 'SR_V2_TG_Noise_Angle_' + str(true_course_angles[ang]) + '_Q' + str(Q_idx)+ '_Nx' + str(N_gm_x) + '_Ny' + str(N_gm_y)
-                save_results(noise_results,filename)  
-                
-                # stop simulation clock
-                Q_idx += 1  
-                
-                tf = time.time() 
-                print ('time taken: '+ str(round(((tf-ti)/60),3)) + ' mins') 
+            print('Running Quardant:' + str(Q_idx) + ' Angle: ' + str(true_course_angle_deg) ) 
+            min_x   = X_LIM[i]
+            max_x   = X_LIM[i+1]
+            max_y   = Y_LIM[j+1] 
+            min_y   = Y_LIM[j] 
+        
+            configs_analyses  = Stopped_Rotor_V2_Analyses.analyses_setup(configs,N_gm_x,N_gm_y,min_y,max_y,min_x,max_x,
+                                               aircraft_range,run_noise_model) 
+            
+            # SET UP MISSION PROFILE 
+            base_mission      = Stopped_Rotor_V2_Missions.approach_departure_mission_setup(configs_analyses,vehicle,simulated_days,flights_per_day,
+                                                    aircraft_range,reserve_segment,control_points,recharge_battery,true_course_angle )
+            missions_analyses = Stopped_Rotor_V2_Missions.missions_setup(base_mission) 
+            
+            # DEFINE ANALYSES 
+            analyses          = SUAVE.Analyses.Analysis.Container()
+            analyses.configs  = configs_analyses
+            analyses.missions = missions_analyses 
+            
+            # FINALIZE SIMULATION 
+            configs.finalize()
+            analyses.finalize()     
+            
+            # APPEND MISSION TO SIMULATION 
+            mission           = analyses.missions.base
+            
+            # RUN SIMULATION !!
+            noise_results     = mission.evaluate()   
+            
+            # SAVE RESULTS
+            filename          = 'SR_V2_TG_Noise_Angle_' + str(true_course_angle_deg) + '_Q' + str(Q_idx)+ '_Nx' + str(N_gm_x) + '_Ny' + str(N_gm_y)
+            save_results(noise_results,filename)  
+            
+            # stop simulation clock
+            Q_idx += 1  
+            
+            tf = time.time() 
+            print ('time taken: '+ str(round(((tf-ti)/60),3)) + ' mins') 
       
     if plot_mission: 
-        Stopped_Rotor_V2.Stopped_Rotor_V2_Plots.plot_results(noise_results,run_noise_model)       
+        Stopped_Rotor_V2_Plots.plot_results(noise_results,run_noise_model)       
                     
     elapsed_range = noise_results.segments[-1].conditions.frames.inertial.position_vector[-1,0] 
     print('Range : ' + str(round(elapsed_range,2)) + ' m  or ' + str(round(elapsed_range/Units.nmi,2)) + ' nmi')
